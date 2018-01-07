886bde556b624999f155e7b3e10fa87d0a9c5a01
nick mullen <nick-mullen@hotmail.co.uk>
2018-01-07 10:41:20 +0000

    added facebok meta tags into plugin

--- my-facebook-tags.php
+++ my-facebook-tags.php
@@ -10,9 +10,27 @@
  */
 
 /**
- *  hook to the head
+ *  hook function to the head
  */
 add_action( 'wp_head', 'my_facebook_tags' );
 function my_facebook_tags(){
-    echo 'hi form the home page';
+    // if page has a single post add meta tags // 
+    if( is_single()){
+    ?>
+      <!-- facebook mata tags -->
+      <meta property="og:title" content="<?php the_title() ?>" />
+      <meta property="og:site_name" content="<?php bloginfo( 'name' ) ?>" />
+      <meta property="og:url" content="<?php the_permalink() ?>" />
+      <meta property="og:description" content="<?php the_excerpt() ?>" />
+      <meta property="og:type" content="article" />
+
+        <?php 
+        // if paot has a thumbnail get src 
+        if(has_post_thumbnail()):
+            $image = wp_get_attachment_image_src(get_post_thumbnail_id(),'large');
+        ?>   
+<mata property="og:image" content="<?php echo $image[0]; ?>" />
+        <?php endif; ?> 
+    <?php
+    }
 }
